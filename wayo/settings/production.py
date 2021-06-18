ea0d73a86b3dc53ae97d1b1c5791eae2c4b1bb05
Navanjane <nkindelpitiya@gmail.com>
2021-06-18 12:26:22 +0530

    production.py edited

--- wayo/settings/production.py
+++ wayo/settings/production.py
@@ -8,7 +8,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': config('DEBUG'),
+        'PORT': config('DB_PORT'),
     }
 }
 
