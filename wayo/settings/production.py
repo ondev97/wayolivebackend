4f50dcc696b5ad52ca432637693242084689636a
Navanjane <nkindelpitiya@gmail.com>
2021-06-18 10:14:14 +0530

    settings files configured

--- wayo/settings/production.py
+++ wayo/settings/production.py
@@ -1,1 +1,13 @@
+from decouple import config
 ALLOWED_HOSTS = ['188.166.229.132']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DEBUG'),
+    }
+}
