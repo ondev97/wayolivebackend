44ccfdb4b6c9b966b1f7f5c6549344fe82d1a460
Navanjane <nkindelpitiya@gmail.com>
2021-06-18 12:34:43 +0530

    production.py edited

--- wayo/settings/production.py
+++ wayo/settings/production.py
@@ -25,8 +25,6 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'Data'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
