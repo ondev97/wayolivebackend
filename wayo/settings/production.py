7321fe4b05004ac91f5cd7194c9e2fa6e1f3a5d6
Navanjane <nkindelpitiya@gmail.com>
2021-06-18 10:34:41 +0530

    production.py edited

--- wayo/settings/production.py
+++ wayo/settings/production.py
@@ -1,4 +1,4 @@
-from decouple import config
+from .base import *
 ALLOWED_HOSTS = ['188.166.229.132']
 
 DATABASES = {
@@ -11,3 +11,22 @@ DATABASES = {
         'PORT': config('DEBUG'),
     }
 }
+
+INSTALLED_APPS = [
+    'storages',
+]
+
+AWS_ACCESS_KEY_ID = 'NMH56U2MV4NVQHBZB4DV'
+AWS_SECRET_ACCESS_KEY = 'p5lvJTqge/owJ83RjHkUfqfNEJSf17gzq6ZcFwbdN9M'
+AWS_STORAGE_BUCKET_NAME = 'wayodatastorage'
+AWS_S3_ENDPOINT_URL = 'https://sgp1.digitaloceanspaces.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'Data'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
