75695568a2bff32c24532b5fea71be3ab813ddd9
navanjane <nkindelpitiya@gmail.com>
2021-06-24 11:16:57 +0530

    is_enrolled added to events

--- show/models.py
+++ show/models.py
@@ -41,6 +41,7 @@ class Event(models.Model):
     is_freeze = models.BooleanField(default=False)
     event_cover = models.ImageField(null=True, blank=True, upload_to=upload_location, default='event_images/default.jpg')
     event_price = models.IntegerField(null=True,blank=True)
+    is_enrolled = models.BooleanField(default=False)
 
     def __str__(self):
         return self.event_name
