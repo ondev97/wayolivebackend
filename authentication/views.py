57d96a2e811bd3de513b603ba7867c3d6c6181e6
Sandeep <sandeepkushaj3@gmail.com>
2021-06-25 13:06:54 +0530

    checkuser view edited

--- authentication/views.py
+++ authentication/views.py
@@ -8,7 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.views import APIView
-
+from rest_framework.authtoken.models import Token
 from show.models import Enrollment
 from .serializer import *
 
