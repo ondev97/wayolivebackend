adae031771f634e979901542123beb97c4eb8116
navanjane <nkindelpitiya@gmail.com>
2021-06-25 20:48:20 +0530

    browsable api disabled

--- wayo/settings/base.py
+++ wayo/settings/base.py
@@ -82,6 +82,9 @@ REST_FRAMEWORK={
     'DEFAULT_AUTHENTICATION_CLASSES':(
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
     )
 }
 
