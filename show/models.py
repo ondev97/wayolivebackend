44acfdbb9a1bbf970815e0edc98311bbf07bb229
navanjane <nkindelpitiya@gmail.com>
2021-07-01 18:21:44 +0530

    AudienceDataForm Added to Admin

--- show/models.py
+++ show/models.py
@@ -87,6 +87,5 @@ class AudienceDataForm(models.Model):
     phone_number = models.CharField(max_length=15,null=True,blank=True)
 
     def __str__(self):
-        return self.first_name+" "+self.last_name
-
+        return self.first_name
 
