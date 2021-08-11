6c250e37e9bca5e073bf3cc4ed16c38267acdea3
Sandeep Kushaj Vithanage <sandeepkushaj3@.com>
2021-08-11 18:20:46 +0530

    latest event view updated

--- show/views.py
+++ show/views.py
@@ -403,7 +403,7 @@ def audiencedatacollect(request):
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+# @permission_classes([IsAuthenticated])
 def latestevent(request):
     event = Event.objects.filter().order_by('-event_date', '-event_start').first()
     if event:
